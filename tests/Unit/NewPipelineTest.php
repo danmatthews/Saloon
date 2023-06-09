78ab9e4e89f8da461a685e667ed2b393d4b3a8e5
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-06-09 21:14:50 +0100

    Working proof of concept

--- tests/Unit/NewPipelineTest.php
+++ tests/Unit/NewPipelineTest.php
@@ -1,35 +1,49 @@
 <?php
 
 use Saloon\Contracts\PendingRequest;
+use Saloon\Exceptions\Request\Statuses\InternalServerErrorException;
 use Saloon\Helpers\NewPipeline;
 use Saloon\Tests\Fixtures\Connectors\TestConnector;
+use Saloon\Tests\Fixtures\Requests\ErrorRequest;
 use Saloon\Tests\Fixtures\Requests\UserRequest;
 
 test('you can process through many pipes', function () {
     $pipes = [
         static function (PendingRequest $pendingRequest, Closure $next) {
-            $sender = $pendingRequest->getConnector()->sender();
+            ray(3);
 
-            return $sender->sendRequest($pendingRequest);
+            try {
+                return $next($pendingRequest);
+            } catch (InternalServerErrorException $exception) {
+                ray('howdy', $exception);
+            }
         },
         static function (PendingRequest $pendingRequest, Closure $next) {
-            return $next($pendingRequest);
-        },
-        static function (PendingRequest $pendingRequest, Closure $next) {
+            ray(1);
+
             $response = $next($pendingRequest);
+
+            $response->throw();
 
             return $response;
         },
         static function (PendingRequest $pendingRequest, Closure $next) {
-            // Send Request
-
+            ray(2);
 
             return $next($pendingRequest);
+        },
+
+        static function (PendingRequest $pendingRequest, Closure $next) {
+            ray(4);
+
+            $sender = $pendingRequest->getConnector()->sender();
+
+            return $sender->sendRequest($pendingRequest);
         },
     ];
 
     $connector = new TestConnector;
-    $pendingRequest = $connector->createPendingRequest(new UserRequest);
+    $pendingRequest = $connector->createPendingRequest(new ErrorRequest());
 
     $pipeline = new NewPipeline($pipes);
     $final = $pipeline->run($pendingRequest);

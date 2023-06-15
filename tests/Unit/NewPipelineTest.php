112f3c7dd191be357f87464cd9e8fee1933ff1e0
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-06-15 16:15:04 +0100

    wip

--- tests/Unit/NewPipelineTest.php
+++ tests/Unit/NewPipelineTest.php
@@ -10,18 +10,35 @@ use Saloon\Tests\Fixtures\Requests\UserRequest;
 test('you can process through many pipes', function () {
     $pipes = [
         static function (PendingRequest $pendingRequest, Closure $next) {
+            ray(1);
+
+            $response = $next($pendingRequest);
+
+            ray(5);
+
+            dd($response);
+        },
+        static function (PendingRequest $pendingRequest, Closure $next) {
+            ray(2);
+
             $pendingRequest->setUrl('https://tests.saloon.dev/api/error');
 
             return $next($pendingRequest);
         },
         static function (PendingRequest $pendingRequest, Closure $next) {
+            ray(3);
+
             try {
                 return $next($pendingRequest);
             } catch (InternalServerErrorException $exception) {
+                ray(6);
+
                 return $exception->getResponse();
             }
         },
         static function (PendingRequest $pendingRequest, Closure $next) {
+            ray(4);
+
             $response = $next($pendingRequest);
             $response->throw();
 
